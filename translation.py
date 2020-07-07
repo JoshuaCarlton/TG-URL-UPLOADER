0347a8364beeb5e91f5e9d5a8cea5622e6add06e
Jᴏsʜᴜᴀ Cᴀʀʟᴛᴏɴ ✞ <56827413+JoshuaCarlton@users.noreply.github.com>
2020-07-07 17:09:53 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -48,7 +48,7 @@ Expires on: 31/12/2020"""
 --------
 Send /me to know current plan details
 
-© @MC_LinkzZ <a href="https://t.me/ShahiR_Ps"༒ ☬Pᴀκκι☬ ༒ ⁪⁬⁮⁮⁮ ‌‌‌‌</a>"""
+© @MC_LinkzZ <a href="https://t.me/ShahiR_Ps">༒ ☬Pᴀκκι☬ ༒ ⁪⁬⁮⁮⁮ ‌‌‌‌</a>"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
